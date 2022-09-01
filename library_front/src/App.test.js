009bc83d20b0bfce96d20b8e0ae5d205bff58f9b
RcleydsonR <rafael.cleydson@gmail.com>
2022-09-01 16:57:12 -0300

    fix tests

--- library_front/src/App.test.js
+++ library_front/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders app top bar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const bibliotecaHeaderElement = screen.getByText("Biblioteca");
+  const perfilHeaderElement = screen.getByText("Perfil");
+  const registroHeaderElement = screen.getByText("Registro");
+  const loginHeaderElement = screen.getByText("Login");
+  expect(bibliotecaHeaderElement).toBeInTheDocument();
+  expect(perfilHeaderElement).toBeInTheDocument();
+  expect(registroHeaderElement).toBeInTheDocument();
+  expect(loginHeaderElement).toBeInTheDocument();
 });
